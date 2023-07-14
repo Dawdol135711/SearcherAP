8b9931b9d64e3419a047bdaf2adda33e23a59492
Dawid <dawid130892@interia.pl>
2023-07-14 10:10:49 +0200

    added functionality to app

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-const input = document.querySelector(".search");
+const search = document.querySelector(".search");
 const items = document.querySelectorAll("li");
 
-console.log(items);
+const searchfunc = (e) => {
+	const text = e.target.value.toLowerCase();
+	
+
+	items.forEach(el => {
+
+        const task = el.textContent
+		if (task.toLowerCase().indexOf(text) !== -1) {
+			el.style.display = "block";
+		} else {
+			el.style.display = "none";
+		}
+	});
+};
+
+search.addEventListener("keyup", searchfunc);
